422f71ca907ff590d875e5092f8e3b2be389c462
Federico Paolinelli <fpaoline@redhat.com>
2025-07-03 15:18:01 +0200

    Revert "Update website from 564e4d6 on publish"
    This reverts commit 919db455b46dbc8eb50d990c8b894b6755248481.

---
+++ README.md
@@ -0,0 +1,57 @@
+## OpenPERouter
+
+Official docs at [openperouter.github.io](https://openperouter.github.io).
+
+OpenPERouter is an open implementation of a Provider Edge (PE) router, designed to terminate multiple VPN protocols on Kubernetes nodes and expose a BGP interface to the host network.
+
+**This project is in the early stage of development. Use carefully!**
+
+## Enable L3 EVPN in your cluster
+
+OpenPERouter enables L3 EVPN tunneling to any BGP enabled Kubernetes component,
+such as Calico, MetalLB, KubeVip, Cilium, FRR-K8s and many others, behaving as an external router.
+
+Behaving as an external router, the integration is seamless and BGP based, exactly as if a physical
+Provider Edge Router was moved inside the node.
+
+## Enable L2 EVPN in the cluster
+
+OpenPERouter supports L2 overlays, allowing seamless communication between nodes using a stretched
+layer 2 domain.
+
+## Overview
+
+Where we normally have a node interacting with the TOR switch, which is configured to map the VLans to a given VPN tunnel,
+OpenPERouter runs directly in the node, exposing one Veth interface per VPN tunnel.
+
+After OpenPERouter is configured and deployed on a cluster, it can interact with any BGP-speaking component of the cluster, including FRR-K8s, MetalLB, Calico and others. The abstraction is as if a physical Provider Edge Router was moved inside the node.
+
+Here is a high level overview of the abstraction, on the left side a classic Kubernetes deployment connected via vlan interfaces, on the right side a deployment of OpenPERouter on a Kubernetes node:
+
+L3:
+
+<img src="website/static/images/openpedescription.svg" alt="L3 Overview" style="width: 80%; max-width: 600px;">
+
+L2:
+
+<img src="website/static/images/openpedescriptionl2.svg" alt="L2 Overview" style="width: 80%; max-width: 600px;">
+
+## Check the documentation
+
+The documentation can be found at [openperouter.github.io](https://openperouter.github.io).
+
+## License
+
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ internal/hostnetwork/bridge.go
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier:Apache-2.0
+
+package hostnetwork
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/vishvananda/netlink"
+)
+
+// setup bridge creates the bridge if not exists, and it enslaves it to the provided
+// vrf.
+func setupBridge(params VNIParams, vrf *netlink.Vrf) (*netlink.Bridge, error) {
+	name := bridgeName(params.VNI)
+	bridge, err := createBridge(name, vrf.Index)
+	if err != nil {
+		return nil, err
+	}
+
+	err = setAddrGenModeNone(bridge)
+	if err != nil {
+		return nil, fmt.Errorf("failed to set addr_gen_mode to 1 for %s: %w", bridge.Name, err)
+	}
+
+	err = netlink.LinkSetUp(bridge)
+	if err != nil {
+		return nil, fmt.Errorf("could not set link up for bridge %s: %v", name, err)
+	}
+	return bridge, nil
+}
+
+// create bridge creates a bridge with the given name, enslaved
+// to the provided vrf.
+func createBridge(name string, vrfIndex int) (*netlink.Bridge, error) {
+
+	toCreate := &netlink.Bridge{LinkAttrs: netlink.LinkAttrs{
+		Name:        name,
+		MasterIndex: vrfIndex,
+	}}
+
+	link, err := netlink.LinkByName(name)
+	// link does not exist, let's create it
+	if errors.As(err, &netlink.LinkNotFoundError{}) {
+		if err := netlink.LinkAdd(toCreate); err != nil {
+			return nil, fmt.Errorf("could not create bridge %s: %w", name, err)
+		}
+		return toCreate, nil
+	}
+	if err != nil {
+		return nil, fmt.Errorf("could find bridge by name %s: %w", name, err)
+	}
+
+	bridge, ok := link.(*netlink.Bridge)
+	if ok && bridge.MasterIndex == vrfIndex { // link exists, nothing to do here
+		return bridge, nil
+	}
+
+	// link exists but it's not a bridge, or wrong vrf. Let's delete and create
+	err = netlink.LinkDel(link)
+	if err != nil {
+		return nil, fmt.Errorf("failed to delete link %v: %w", link, err)
+	}
+	if err := netlink.LinkAdd(toCreate); err != nil {
+		return nil, fmt.Errorf("could not create bridge %s: %w", name, err)
+	}
+
+	return toCreate, nil
+}
+
+const (
+	macSize = 6
+)
+
+var macHeader = []byte{0x00, 0xF3}
+
+func setBridgeFixedMacAddress(bridge netlink.Link, vni int) error {
+	macAddress := make([]byte, macSize)
+
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, int32(vni+1))
+	if err != nil {
+		return err
+	}
+	copy(macAddress, macHeader)
+	copy(macAddress[2:], buf.Bytes())
+	if err := netlink.LinkSetHardwareAddr(bridge, macAddress); err != nil {
+		return fmt.Errorf("failed to set mac address to bridge %s %x: %w", bridge.Attrs().Name, macAddress, err)
+	}
+	return nil
+}
+
+const bridgePrefix = "br-pe-"
+
+func bridgeName(vni int) string {
+	return fmt.Sprintf("%s%d", bridgePrefix, vni)
+}
+
+func vniFromBridgeName(name string) (int, error) {
+	if !strings.HasPrefix(name, bridgePrefix) {
+		return 0, NotRouterInterfaceError{Name: name}
+	}
+
+	vni := strings.TrimPrefix(name, bridgePrefix)
+	res, err := strconv.Atoi(vni)
+	if err != nil {
+		return 0, fmt.Errorf("failed to get vni for bridge %s", name)
+	}
+	return res, nil
+}
