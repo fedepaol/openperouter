a93acaf84e543a954805cdd22fde2428944b9a44
Federico Paolinelli <fedepaol@gmail.com>
2025-10-26 11:05:19 +0100

    wipwip

--- systemdmode/generate_systemd.sh
+++ systemdmode/generate_systemd.sh
@@ -57,6 +57,7 @@ podman create --pod=controllerpod --name=controller \
 	-v /var/lib/hostambassador:/shared:rshared \
 	-v=/proc:/hostproc:ro \
 	-e KUBECONFIG=/shared/kubeconfig \
+	--privileged \
 	--network=host \
 	--cap-add=CAP_NET_BIND_SERVICE,CAP_NET_ADMIN,CAP_NET_RAW,CAP_SYS_ADMIN \
 	--pid=host \
